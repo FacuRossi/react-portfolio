fe59e7827345ee6b91a2da3504736e6a4b54ec45
Facundo Rossi <rossifacundo94@gmail.com>
2019-02-02 15:30:02 -0300

    Add content main page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+const occupation = "I'm a software engineer based in Córdoba, Argentina. I'm really enjoying the challenges that my profession involve, I take it as a great opportunity for continuing to grow as a person and professional."
+
 function Home(props) {
   return (
     <div className="home">
-      <p>HOME</p>
+      <div className="animate animationOne">
+        <span>F</span>
+        <span>a</span>
+        <span>c</span>
+        <span>u</span>
+        <span>n</span>
+        <span>d</span>
+        <span>o</span>&nbsp;
+				<span>R</span>
+        <span>o</span>
+        <span>s</span>
+        <span>s</span>
+        <span>i</span>
+      </div>
+      <span className="banner-text">
+        {occupation}
+      </span>
     </div>
   )
 }
